1652d2de9986755af7bcf81b9e3d7ff0db546507
JackyLui1215 <343344461@tdsb.ca>
2026-03-27 14:27:52 +0000

    finisehd

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,33 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	
+	public static String evenOrOdd(int number) {
+		String evenOrOdd ="Odd";
+		if ((number%2) == 0) {
+			 evenOrOdd = "Even";
+		}
+		return evenOrOdd;
+	      
+	}
+
+	public static String teacherOrStudent(String name) {
+		String teacherOrStudent = "Student";
+		if(name == "Kalisz") {
+        teacherOrStudent = "Teacher";
+		}
+		return teacherOrStudent;
+	}
+
+	public static int fartherFromZero(int number) {
+		int fartherFromZero = 0;
+		if(number > 0) {
+			fartherFromZero = (number + 5);
+		}
+        if(number < 0) {
+			fartherFromZero = (number - 5);
+		}
+		return fartherFromZero;
+	}
 
 }
